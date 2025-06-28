f246d07d01ecb4a3b3a5bff4324cbf097b16d0f3
Murat Ansarov <muratansarov302@gmail.com>
2025-06-28 19:50:10 +0500

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.Base_URL}>
+  <BrowserRouter basename="/portfolio">
     <App />
   </BrowserRouter>,
 )
